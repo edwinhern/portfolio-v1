c49d52826baa462332f3515bed8c77eb18e01417
Edwin Hernandez <edwinhern.16@gmail.com>
2024-01-31 14:36:00 -0600

    refactor: config files and eslint checks

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [{ hostname: 'cdn.sanity.io' }, { hostname: 'source.unsplash.com' }],
+  },
+  typescript: {
+    // Set this to false if you want production builds to abort if there's type errors
+    ignoreBuildErrors: process.env.VERCEL_ENV === 'production',
+  },
+  eslint: {
+    /// Set this to false if you want production builds to abort if there's lint errors
+    ignoreDuringBuilds: process.env.VERCEL_ENV === 'production',
+  },
+};
+
+export default bundleAnalyzer(nextConfig);
