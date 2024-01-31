ad14221f55fed66910dfcf0dc79491896173e93a
Edwin Hernandez <edwinhern.16@gmail.com>
2024-01-31 14:59:11 -0600

    fix: eslintignore and next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,17 +6,7 @@ const bundleAnalyzer = withBundleAnalyzer({
 
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [{ hostname: 'cdn.sanity.io' }, { hostname: 'source.unsplash.com' }],
-  },
-  typescript: {
-    // Set this to false if you want production builds to abort if there's type errors
-    ignoreBuildErrors: process.env.VERCEL_ENV === 'production',
-  },
-  eslint: {
-    /// Set this to false if you want production builds to abort if there's lint errors
-    ignoreDuringBuilds: process.env.VERCEL_ENV === 'production',
-  },
+  images: { remotePatterns: [{ hostname: 'cdn.sanity.io' }, { hostname: 'source.unsplash.com' }] },
 };
 
 export default bundleAnalyzer(nextConfig);
