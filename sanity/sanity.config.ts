f4e278ca3644e5098fb4b909df6f80e0b7494d73
Edwin Hernandez <edwinhern.16@gmail.com>
2024-01-31 21:19:57 -0600

    refactor: unused dependencies and API routes

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -3,8 +3,6 @@ import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { unsplashImageAsset } from 'sanity-plugin-asset-source-unsplash';
-import Iframe, { defineUrlResolver, IframeOptions } from 'sanity-plugin-iframe-pane';
-import { previewUrl } from 'sanity-plugin-iframe-pane/preview-url';
 
 import { apiVersion, dataset, previewSecretId, projectId } from '@/sanity/lib/api';
 import post from '@/sanity/schemas/post';
@@ -17,16 +15,6 @@ export const PREVIEWABLE_DOCUMENT_TYPES_REQUIRING_SLUGS = [post.name] satisfies 
 
 // Used to generate URLs for drafts and live previews
 export const PREVIEW_BASE_URL = '/api/preview';
-
-export const urlResolver = defineUrlResolver({
-  base: PREVIEW_BASE_URL,
-  requiresSlug: PREVIEWABLE_DOCUMENT_TYPES_REQUIRING_SLUGS,
-});
-
-export const iframeOptions = {
-  url: urlResolver,
-  urlSecretId: previewSecretId,
-} satisfies IframeOptions;
 
 export default defineConfig({
   basePath: '/admin',
@@ -41,19 +29,11 @@ export default defineConfig({
           return S.document().views([
             // Default form view
             S.view.form(),
-            // Preview
-            S.view.component(Iframe).options(iframeOptions).title('Preview'),
           ]);
         }
 
         return null;
       },
-    }),
-    previewUrl({
-      base: PREVIEW_BASE_URL,
-      requiresSlug: PREVIEWABLE_DOCUMENT_TYPES_REQUIRING_SLUGS,
-      urlSecretId: previewSecretId,
-      matchTypes: PREVIEWABLE_DOCUMENT_TYPES,
     }),
     unsplashImageAsset(),
     visionTool({ defaultApiVersion: apiVersion }),
