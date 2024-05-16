9faa64f22338000d6e7b9c0c6722a997a88d5025
Edwin Hernandez <edwinhern.16@gmail.com>
2024-05-15 22:19:32 -0500

    chore: :green_heart: skip Husky hooks in release workflow and update release configuration

--- release.config.cjs
+++ release.config.cjs
@@ -13,7 +13,7 @@ module.exports = {
       '@semantic-release/git',
       {
         assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
-        message: 'chore(release): :bookmark: bump version ${nextRelease.version}',
+        message: 'chore(release): :bookmark: bump version ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
   ],
